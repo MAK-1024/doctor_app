1e6fd6f402ae835dd11a7e4cb8c0db1e48326070
MAK <moukhtar206612@gmail.com>
2024-11-24 21:46:26 +0200

    add login screen

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -3,6 +3,8 @@
  class Routes {
   static const String onBoardingScreen = "/onBoardingScreen";
   static const String loginScreen = "/loginScreen";
+  static const String signUpScreen = "/signUpScreen";
+
 
 
  }
